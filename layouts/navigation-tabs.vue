3e6733deb6284842f924951559c1a2acd106e127
mohamed jamoun <mjamoun28@gmail.com>
2021-07-18 12:20:12 +0100

    </>

---
+++ layouts/navigation-tabs.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-row class="fill-height" no-gutters>
+        <v-col cols="12" md="4" lg="3">
+          <v-card flat tile class="lightGrey fill-height overflow-hidden">
+            <v-row class="py-0 saide-wall-logo text-center" no-gutters>
+              <v-col class="d-flex justify-center">
+                <img src="~/assets/img/logo.svg" />
+              </v-col>
+              <v-col cols="12" class="mt-12">
+                <v-btn
+                  icon
+                  x-large
+                  nuxt
+                  to="/"
+                  active-class="primary white--text"
+                >
+                  1
+                </v-btn>
+              </v-col>
+              <v-col cols="12" class="my-3">
+                <v-divider vertical />
+              </v-col>
+              <v-col cols="12">
+                <v-btn icon x-large>2</v-btn>
+              </v-col>
+              <v-col cols="12" class="my-3">
+                <v-divider vertical />
+              </v-col>
+              <v-col cols="12">
+                <v-btn icon x-large>3</v-btn>
+              </v-col>
+            </v-row>
+            <img
+              src="~/assets/img/saide-wall-design.svg"
+              class="saide-wall-design"
+            />
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="8" lg="9">
+          <v-card
+            flat
+            tile
+            style="min-height: 100%"
+            class="d-flex align-center justify-center"
+          >
+            <Nuxt />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.saide-wall-design {
+  width: 100%;
+  position: absolute;
+  bottom: -30px;
+  left: 0;
+  right: 0;
+  width: 100%;
+}
+.saide-wall-logo {
+  width: 100%;
+  position: absolute;
+  top: 47px;
+  a,
+  button {
+    background-color: white;
+    span {
+      font-weight: 700;
+      font-size: 18px;
+    }
+  }
+  .v-divider {
+    border: none;
+    background-color: white;
+    opacity: 1;
+    max-width: 3px;
+    width: 3px;
+    height: 43px;
+  }
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: "%s - rainwalk",
+    title: "rainwalk",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~assets/sass/index.scss"],
+
+  styleResources: {
+    scss: ["~/assets/sass/variables.scss"]
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify",
+    "@nuxtjs/style-resources"
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    "@nuxtjs/axios",
+    // https://go.nuxtjs.dev/pwa
+    "@nuxtjs/pwa",
+    "nuxt-webfontloader"
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: "en"
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ["~/assets/sass/variables.scss"],
+    treeShake: true,
+    theme: {
+      options: { customProperties: true },
+      themes: {
+        light: {
+          primary: "#155F6E",
+          accent: "#57B894",
+          secondary: "#B8567B",
+          lightGrey: "#DADADA",
+          info: "#FFF",
+          warning: "#FFF",
+          error: "#B8567B",
+          success: "#FFF"
+        }
+      }
+    },
+    defaultAssets: false
+  },
+
+  webfontloader: {
+    google: {
+      families: ["Open+Sans:200,400,700,800,900"]
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+};
